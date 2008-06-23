cfd472a1f9ea047dfcc4888cd4614da32d8a4006
Pedro Melo <melo@simplicidade.org>
2008-06-23 12:48:40 +0100

    Added initial version of the bash-completion script for chit
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

---
+++ chit-bash-completion.sh
@@ -0,0 +1,25 @@
+#
+# completion rules for chit
+#
+# See http://github.com/robin/chit/ for more information about chit
+# and http://github.com/melo/chit/ for the main development branch of this
+# bash completion file
+#
+# Very basic stuff, fork and fix :)
+#
+# Written by Pedro Melo <melo@simplicidade.org>, June 23, 2008
+#
+
+function _chit_completion()
+{
+  local chits partial
+
+  chits=$( chit all 2>/dev/null )
+  partial=${COMP_WORDS[COMP_CWORD]}
+
+  COMPREPLY=( $( compgen -W "$chits" -- $partial ) )
+
+  return 0
+}
+
+complete -F _chit_completion -o default chit
